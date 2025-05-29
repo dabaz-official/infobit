323b42b5fa6544d5faf61e919c3fdcd9da11b66f
DabAZ <dabaz@dabaz.me>
2025-05-30 01:47:31 +0800

    Add styles for KaTeX

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,7 @@ import { Banner, Head } from 'nextra/components'
 import { getPageMap } from 'nextra/page-map'
 import 'nextra-theme-docs/style.css'
 import cn from 'clsx'
+import 'katex/dist/katex.min.css'
 
 import { InfoBitWordmark, DabAZWordmark } from './wordmark'
  
